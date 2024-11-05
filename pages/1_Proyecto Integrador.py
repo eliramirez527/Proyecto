1eed492340e3eac83c862213071c5705d59d4911
ElianaRamirez <118145292+eliramirez527@users.noreply.github.com>
2024-11-04 20:44:42 -0500

    Update 1_Proyecto Integrador.py. cam 1

--- pages/1_Proyecto Integrador.py
+++ pages/1_Proyecto Integrador.py
@@ -1,20 +1,25 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
-import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Configuración de la página de Streamlit
 st.set_page_config(layout="wide")
+
+st.sidebar.title("Nuevas Tecnologias")
+
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
 st.title("Explorador de Base de Datos de Canciones")
 
-df = pd.read_csv ('datasets/songs_data (1).csv')
+df = pd.read_csv('static/datasets/songs_data (1).csv')
 
 artists = sorted(df['Nombre del artista'].unique())
 genres = sorted(df['Género/s musical/es'].unique())
 years = sorted(df['Fecha de lanzamiento'].apply(lambda x: x.split('-')[0]).unique())
-      
+
 def filter_by_genre(df, genres):
-        return df[df['Género/s musical/es'].isin(genres)]
+    return df[df['Género/s musical/es'].isin(genres)]
 
 def filter_by_artist(df, artist):
     return df[df['Nombre del artista'] == artist]
@@ -23,20 +28,23 @@ def filter_by_album(df, album):
     return df[df['Álbum'] == album]
 
 def filter_by_song(df, song):
-     return df[df['Título de la canción'] == song]
+    return df[df['Título de la canción'] == song]
 
 def filter_by_album(df, album_title):
     return df[df['Título del álbum'] == album_title]
 
 def filter_by_release_year(df, start_year, end_year):
-        df['Fecha de lanzamiento'] = pd.to_datetime(df['Fecha de lanzamiento'])
-        return df[(df['Fecha de lanzamiento'].dt.year >= start_year) & (df['Fecha de lanzamiento'].dt.year <= end_year)]
+    df['Fecha de lanzamiento'] = pd.to_datetime(df['Fecha de lanzamiento'])
+    return df[(df['Fecha de lanzamiento'].dt.year >= start_year) & (df['Fecha de lanzamiento'].dt.year <= end_year)]
 
 def filter_by_reproductions(df, min_reproductions, max_reproductions):
     return df[(df['Número de reproducciones'] >= min_reproductions) & (df['Número de reproducciones'] <= max_reproductions)]
 
 def filter_by_top5(df):
     return df[df['Top 5'] == True]
+
+# Configurar la paleta de colores de Seaborn
+sns.set_palette("Set2")
 
 # Filtro por género musical
 def filtro1():
@@ -45,10 +53,12 @@ def filtro1():
         filtered_df = filter_by_genre(df, selected_genres)
         st.dataframe(filtered_df)
         st.subheader("Gráfico de Canciones por Género")
-        fig = go.Figure(data=[
-            go.Bar(x=filtered_df['Género/s musical/es'].value_counts().index, y=filtered_df['Género/s musical/es'].value_counts().values)
-        ])
-        st.plotly_chart(fig, use_container_width=True)
+        
+        plt.figure(figsize=(10, 5))
+        sns.countplot(data=filtered_df, y='Género/s musical/es', order=filtered_df['Género/s musical/es'].value_counts().index)
+        plt.xlabel('Número de Canciones')
+        plt.ylabel('Género Musical')
+        st.pyplot(plt.gcf())
 
 # Filtro por artista
 def filtro2():
@@ -69,10 +79,13 @@ def filtro2():
                 filtered_df_song = filter_by_song(filtered_df_album, selected_song)
                 st.dataframe(filtered_df_song)
                 st.subheader(f"Gráfico de Reproducciones del Artista: {selected_artist} - Álbum: {selected_album} - Canción: {selected_song}")
-                fig = go.Figure(data=[
-                    go.Bar(x=filtered_df_song['Título de la canción'], y=filtered_df_song['Número de reproducciones'])
-                ])
-                st.plotly_chart(fig, use_container_width=True)
+                
+                plt.figure(figsize=(10, 5))
+                sns.barplot(data=filtered_df_song, x='Título de la canción', y='Número de reproducciones', palette="Set2")
+                plt.xlabel('Canción')
+                plt.ylabel('Número de Reproducciones')
+                st.pyplot(plt.gcf())
+
                 st.subheader("Promedio de Reproducciones")
                 promedio_reproducciones = filtered_df_song['Número de reproducciones'].mean()
                 st.subheader(round(promedio_reproducciones, 1))
@@ -83,10 +96,12 @@ def filtro3():
     filtered_df = filter_by_release_year(df, start_year, end_year)
     st.dataframe(filtered_df)
     st.subheader("Gráfico de Canciones por Año de Lanzamiento")
-    fig = go.Figure(data=[
-        go.Bar(x=filtered_df['Fecha de lanzamiento'].dt.year.value_counts().index, y=filtered_df['Fecha de lanzamiento'].dt.year.value_counts().values)
-    ])
-    st.plotly_chart(fig, use_container_width=True)
+    
+    plt.figure(figsize=(10, 5))
+    sns.countplot(data=filtered_df, x=filtered_df['Fecha de lanzamiento'].dt.year, order=sorted(filtered_df['Fecha de lanzamiento'].dt.year.unique()), palette="Set2")
+    plt.xlabel('Año de Lanzamiento')
+    plt.ylabel('Número de Canciones')
+    st.pyplot(plt.gcf())
 
 # Filtro por número de reproducciones
 def filtro4():
@@ -94,20 +109,24 @@ def filtro4():
     filtered_df = filter_by_reproductions(df, min_reproductions, max_reproductions)
     st.dataframe(filtered_df)
     st.subheader("Gráfico de Reproducciones")
-    fig = go.Figure(data=[
-        go.Bar(x=filtered_df['Título de la canción'], y=filtered_df['Número de reproducciones'])
-    ])
-    st.plotly_chart(fig, use_container_width=True)
+    
+    plt.figure(figsize=(10, 5))
+    sns.barplot(data=filtered_df, x='Título de la canción', y='Número de reproducciones', palette="Set2")
+    plt.xlabel('Canción')
+    plt.ylabel('Número de Reproducciones')
+    st.pyplot(plt.gcf())
 
 # Filtro por Top 5
 def filtro5():
     filtered_df = filter_by_top5(df)
     st.dataframe(filtered_df)
     st.subheader("Gráfico de Top 5 Canciones")
-    fig = go.Figure(data=[
-        go.Bar(x=filtered_df['Título de la canción'], y=filtered_df['Número de reproducciones'])
-    ])
-    st.plotly_chart(fig, use_container_width=True)
+    
+    plt.figure(figsize=(10, 5))
+    sns.barplot(data=filtered_df, x='Título de la canción', y='Número de reproducciones', palette="Set2")
+    plt.xlabel('Canción')
+    plt.ylabel('Número de Reproducciones')
+    st.pyplot(plt.gcf())
 
 # Menú de selección de filtro
 filtros = [
